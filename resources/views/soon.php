2e87d1c84786453cf24bd7c4018343f7fd6194f7
Esqulo <zecazeca11@hotmail.com>
2024-08-22 21:04:34 -0300

    fix view `soon.php` imports

--- resources/views/soon.php
+++ resources/views/soon.php
@@ -2,13 +2,13 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" type="image/x-icon" href="/assets/logo.png"/>
+    <link rel="icon" type="image/x-icon" href="../../assets/logo.png"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site em Construção</title>
     <style>
         @font-face {
             font-family: 'LexendExa-Regular';
-            src: url('/assets/fonts/LexendExa-Regular.ttf');
+            src: url('../../assets/fonts/LexendExa-Regular.ttf');
             font-weight: normal;
             font-style: normal;
         }
@@ -73,7 +73,7 @@
 <body>
     <div class="container">
         <div class="logo">
-            <img src="/assets/logo.png" alt="Logo">
+            <img src="../../assets/logo.png" alt="Logo">
         </div>
         <h1>Site em Construção</h1>
         <p>Estamos trabalhando para trazer algo incrível. Volte em breve!</p>
