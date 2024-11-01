d4fab66e79be17e6b5a80337c25daac37c1cc93d
Esqulo <zecazeca11@hotmail.com>
2024-10-31 21:36:40 -0300

    `answer` on `deals` is now correctly set as nullable

--- database/migrations/2024_08_27_025614_transactions.php
+++ database/migrations/2024_08_27_025614_transactions.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->unsignedBigInteger('to');
             $table->foreign('to')->references('id')->on('users')->onDelete('no action');
             $table->dateTime('created_at')->useCurrent();
-            $table->integer('answer');
+            $table->integer('answer')->nullable();
             $table->dateTime('answered_at');
             $table->unsignedBigInteger('counter_next')->nullable();
             $table->foreign('counter_next')->references('id')->on('deals')->onDelete('no action');
