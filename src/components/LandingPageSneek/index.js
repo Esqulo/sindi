f5c9fd467fc34475ebf69b004dd6f85c7e9da587
Esqulo <zecazeca11@hotmail.com>
2024-11-23 22:41:53 -0300

    add laptop component

---
+++ src/components/LandingPageSneek/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./styles.css";
+
+import LaptopScreenHome from "../../assets/images/landing_page/laptop-images/home.png"
+import LaptopScreenProfile from "../../assets/images/landing_page/laptop-images/perfil-sindico.png"
+
+function LandingPageSneek(){
+
+    const [currentImage, setCurrentImage] = useState(0);
+    const images = [LaptopScreenHome,LaptopScreenProfile];
+    
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+          setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+        }, 8000);
+    
+        return () => clearInterval(interval);
+
+    },[images.length]);
+
+    return (
+        <div className="sneek-container">
+            <span className="sneek-title">Quer saber como está ficando? Dê uma olhadinha</span>
+            <div className="sneek-laptop">
+                <div className="sneek-laptopscreen" style={{backgroundImage: `url(${images[currentImage]})`}}></div>
+            </div>
+        </div>
+    );
+}
+
+export default LandingPageSneek;
--- src/routes/LandingPage/index.js
+++ src/routes/LandingPage/index.js
@@ -3,12 +3,14 @@ import React from "react";
 import Banner from "../../components/LandingPageBanner"
 import LandingPageContent from "../../components/LandingPageContent";
 import SocialMediaIcons from "../../components/SocialMediaIcons"
+import Sneek from "../../components/LandingPageSneek"
 
 function LandingPage(){
     return (
         <div className="lp-container">
             <Banner/>
             <LandingPageContent/>
+            <Sneek/>
             <SocialMediaIcons/>
         </div>
     );
