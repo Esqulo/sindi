acb1e4e7596bde04d5128648c4298d57a4099655
Esqulo <zecazeca11@hotmail.com>
2024-11-24 10:29:28 -0300

    disable temporary migration

--- services/database/migrations/2024_10_23_230835_subscriptions.php
+++ services/database/migrations/2024_10_23_230835_subscriptions.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('subscriptions', function (Blueprint $table) {
-        });
-
+        // Schema::create('subscriptions', function (Blueprint $table) {
+        // });
     }
 
     /**
