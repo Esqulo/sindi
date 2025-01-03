4c3bdaef638d62da501494d25fd09cd73c714d52
Esqulo <zecazeca11@hotmail.com>
2025-01-02 22:05:06 -0300

    create a chevron component

--- src/components/Chevron/index.js
+++ src/components/Chevron/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import "./styles.css";
 
-function Chevron({turn=0}){
-    return(
-        <p>Chevron (top) <span class="chevron"></span></p>
-    )  
-    // <span class="custom-chevron" style={{transform: `rotate(${turn}deg)`}}></span>
+function Chevron({rotate="45deg", width="1px", size="10px", color="#000"}){
+    return (
+        <span 
+            className="chevron"
+            style={{
+                '--border-width': `${width} ${width} 0 0`,
+                '--size': size,
+                '--rotate': rotate,
+                '--color': color,
+            }}
+        />
+    )
 }
 
 export default Chevron
