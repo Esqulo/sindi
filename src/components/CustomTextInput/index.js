b0db4bdd35fcf78fa96339a7bfe5d8e4e94560ee
Esqulo <zecazeca11@hotmail.com>
2025-01-28 20:27:00 -0300

    add mask to `CustomTextInput`

--- src/components/CustomTextInput/index.js
+++ src/components/CustomTextInput/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./styles.css";
+import MaskedInput from "react-input-mask";
 
-function CustomTextInput({name, label, placeholder, type="text", required=false, onChange, inputRef, errorMessage, customStyle}) {
+function CustomTextInput({name, label, placeholder, type="text", required=false, onChange, inputRef, errorMessage, customStyle, mask}) {
     return (
         <div className="custom-text-input">
             <label className="custom-text-input-label">{label}</label>
-            <input className="custom-text-input-field" name={name} type={type} placeholder={placeholder} required={required} onChange={(e) => onChange(e.target.value)} ref={inputRef}/>
+            <MaskedInput 
+                className="custom-text-input-field"
+                name={name} type={type}
+                placeholder={placeholder}
+                required={required}
+                mask={mask}
+                onChange={(e) => onChange(e.target.value)}
+                ref={inputRef}
+            />
             {errorMessage && <span className="custom-text-input-error">{errorMessage}</span>}
         </div>
     )
