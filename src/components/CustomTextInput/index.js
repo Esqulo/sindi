38905eb4df331e9d91f10a630e231d947a3c7441
Esqulo <zecazeca11@hotmail.com>
2025-02-01 17:24:43 -0300

    Added `value`, `disabled` and `customStyle` features do `CustomTextInput`

--- src/components/CustomTextInput/index.js
+++ src/components/CustomTextInput/index.js
@@ -2,19 +2,22 @@ import React from "react";
 import "./styles.css";
 import MaskedInput from "react-input-mask";
 
-function CustomTextInput({name, label, placeholder, type="text", required=false, onChange, inputRef, errorMessage, customStyle, mask}) {
+function CustomTextInput({name, value, label, placeholder, type="text", required=false, onChange, inputRef, errorMessage, customStyle, mask, disabled}) {
     return (
         <div className="custom-text-input">
             <label className="custom-text-input-label">{label}</label>
             <MaskedInput 
                 className="custom-text-input-field"
                 name={name}
+                value={value}
                 type={type}
                 placeholder={placeholder}
+                disabled={disabled}
                 required={required}
                 mask={mask}
                 onChange={(e) => onChange(e.target.value)}
                 ref={inputRef}
+                style={customStyle}
             />
             {errorMessage && <span className="custom-text-input-error">{errorMessage}</span>}
         </div>
