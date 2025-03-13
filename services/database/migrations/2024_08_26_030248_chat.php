3bea632222e5b0b56c8238e1d2cc37116fb68922
Esqulo <zecazeca11@hotmail.com>
2025-03-13 00:12:03 -0300

    new chat migration

--- services/database/migrations/2024_08_26_030248_chat.php
+++ services/database/migrations/2024_08_26_030248_chat.php
@@ -13,14 +13,28 @@ return new class extends Migration
     {
         Schema::create('chat', function (Blueprint $table) {
             $table->id();
+            $table->string('title',100)->nullable();
+            $table->integer('type');
+            $table->string('image')->nullable();
+            $table->dateTime('created_at')->useCurrent();
+        });
+
+        Schema::create('chat_messages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('chat_id');
+            $table->foreign('chat_id')->references('id')->on('chat')->onDelete('no action');
             $table->unsignedBigInteger('from');
             $table->foreign('from')->references('id')->on('users')->onDelete('no action');
-            $table->unsignedBigInteger('to');
-            $table->foreign('to')->references('id')->on('users')->onDelete('no action');
             $table->dateTime('sent_at')->useCurrent();
-            $table->dateTime('received_at')->nullable();
-            $table->dateTime('read_at')->nullable();
             $table->string('message',2000);
+        });
+
+        Schema::create('chat_access', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('no action');
+            $table->unsignedBigInteger('chat_id');
+            $table->foreign('chat_id')->references('id')->on('chat')->onDelete('no action');
         });
     }
 
