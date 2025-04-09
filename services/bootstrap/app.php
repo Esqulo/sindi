146dc0be3e001256123d5820e5da2a4b7ab01b85
Esqulo <zecazeca11@hotmail.com>
2025-04-08 21:06:14 -0300

    register `CheckPendingDeals` middleware globally

--- services/bootstrap/app.php
+++ services/bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use App\Http\Middleware\CheckPendingDeals;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -12,7 +14,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        //create a group to use the middleware
+        // $middleware->appendToGroup('PendingDealsGroup', [
+        //     CheckPendingDeals::class
+        // ]);
+
+        // to use globally on /api
+        $middleware->api(prepend: [
+            CheckPendingDeals::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
