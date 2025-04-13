f624a7ba31d353236a741083325bd039dd8fef05
Esqulo <zecazeca11@hotmail.com>
2025-04-13 19:13:48 -0300

    simplify component

--- src/components/Profile/Detail/index.js
+++ src/components/Profile/Detail/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import avatar from '../../../assets/images/profile/avatar.jpg';
+import noUserImage from "../../../assets/images/icons/no-image-profile.png";
 import star from '../../../assets/images/profile/star.png';
 import badge from '../../../assets/images/profile/badge.png';
 
@@ -9,41 +9,37 @@ import "./styles.css";
 function DetailComponent({ userData }) {
 
 	return (
-		<div className='profile-detail-content'>
+		<div className='profile-detail-container'>
 
-			<div className='avatar-content'>
-				<div className='avatar-area'>
-					<img className="avatar-img" src={avatar} alt="Avatar" />
-					<img className="avatar-seal" src={badge} alt="Selo" />
-				</div>
+			<div className='profile-detail-avatar'>
+				<img className='profile-details-avatar-image' src={userData.avatar || noUserImage} alt="user-picture" />
+				{userData.highlight && 
+					<img className="profile-detail-avatar-highlight" src={badge} alt="selo" />
+				}
 			</div>
 
-			<div className='profile-detail'>
-				<div className='title-container'>
-					<div className='name'>
-						<h1>{userData.name}</h1>
-					</div>
+			<div className='profile-detail-userdata'>
+				<span className='profile-detail-userdata-name'>
+					{userData.name}
+				</span>
 
-					<div className='star'>
-						<img src={star} alt="Estrela" /> {userData.stars} (267)
-					</div>
-
-					<div className='profile-info'>
+				<div className='profile-detail-userdata-rating row-centered'>
+					<img src={star} alt="Estrela" /> {userData.rating} ({userData.reviews})
+				</div>
+				
+				{/* future feature
+					<div className='profile-detail-userdata-skills'>
 						<ul>
 							<li>8 anos como síndico profissional</li>
 							<li>Formando em Administração</li>
 							<li>47 anos • 15 km de você</li>
 						</ul>
-					</div>
-				</div>
+					</div> 
+				*/}
 
-				<div className='biografia'>
-					<p className="description">
-						<b>
-							{userData.bio}
-						</b>
-					</p>
-				</div>
+				<span className='profile-detail-bio'>
+					{userData.bio}
+				</span>
 
 			</div>
 		</div>
