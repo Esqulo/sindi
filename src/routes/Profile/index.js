5b91619c007022fbe8a382201969cd1018660cb6
Esqulo <zecazeca11@hotmail.com>
2025-04-13 23:05:23 -0300

    disable future features

--- src/routes/Profile/index.js
+++ src/routes/Profile/index.js
@@ -3,15 +3,15 @@ import "./styles.css";
 
 import BannerComponent from '../../components/Profile/Banner';
 import DetailComponent from '../../components/Profile/Detail';
-import CustomCarousel from '../../components/CustomCarousel';
+// import CustomCarousel from '../../components/CustomCarousel';
+import Reviews from '../../components/Profile/Reviews';
+import Comments from '../../components/Profile/Comments';
 
 import banner from '../../assets/images/profile/banner-profile.png';
-import certificate from '../../assets/images/profile/portfolio/certificate.jpg';
-import portfolio_img from '../../assets/images/profile/portfolio/img001.jpg';
 
 function Profile() {
 
-	const usuario = {
+	const userData = {
 		id: 1,
 		name: "Usuário Teste",
 		rating: 5.89,
@@ -21,39 +21,18 @@ function Profile() {
 		bio: "Os condomínios hoje em dia são como empresas, então ter um profissional competente cuidando do seu imóvel é essencial para o bom funcionamento deste. Sou um profissional competente, empenhado, sério e extremamente engajado com meus condomínios. Tenho certificação em diversas áreas como Economia, Direito, Administração e Contabilidade. Me sinto capaz de gerir diversos condomínios. Mande agora uma proposta e vamos conversar! Os condomínios hoje em dia são como empresas, então ter um profissional competente cuidando do seu imóvel é essencial para o bom funcionamento deste. Sou um profissional competente, empenhado, sério e extremamente engajado com meus condomínios. Tenho certificação em diversas áreas como Economia, Direito, Administração e Contabilidade. Me sinto capaz de gerir diversos condomínios. Mande agora uma proposta e vamos conversar! Os condomínios hoje em dia são como empresas, então ter um profissional competente cuidando do seu imóvel é essencial para o bom funcionamento deste. Sou um profissional competente, empenhado, sério e extremamente engajado com meus condomínios. Tenho certificação em diversas áreas como Economia, Direito, Administração e Contabilidade. Me sinto capaz de gerir diversos condomínios. Mande agora uma proposta e vamos conversar!"
 	}
 
-	const certificates = [
-		{ certificate_id: 1, image: certificate, name: "Certificado 1" },
-		{ certificate_id: 2, image: certificate, name: "Certificado 2" },
-		{ certificate_id: 3, image: certificate, name: "Certificado 3" },
-		{ certificate_id: 4, image: certificate, name: "Certificado 4" },
-		{ certificate_id: 5, image: certificate, name: "Certificado 5" },
-	];
-
-	const portfolio = [
-		{ certificate_id: 1, image: portfolio_img, name: "Certificado 1" },
-		{ certificate_id: 2, image: portfolio_img, name: "Certificado 2" },
-		{ certificate_id: 3, image: portfolio_img, name: "Certificado 3" },
-		{ certificate_id: 4, image: portfolio_img, name: "Certificado 4" },
-		{ certificate_id: 5, image: portfolio_img, name: "Certificado 5" },
-		{ certificate_id: 6, image: portfolio_img, name: "Certificado 6" },
-		{ certificate_id: 7, image: portfolio_img, name: "Certificado 7" },
-		{ certificate_id: 8, image: portfolio_img, name: "Certificado 8" },
-		{ certificate_id: 9, image: portfolio_img, name: "Certificado 9" },
-		{ certificate_id: 10, image: portfolio_img, name: "Certificado 10" },
-	];
-
 	return (
 		<div className='home-container column-centered shadow-default'>
 
 			<BannerComponent bannerImg={banner} />
-			<DetailComponent userData={usuario} />
+			<DetailComponent userData={userData} />
 
-			<CustomCarousel title={"Portfólio"} items={portfolio}/>
-			<CustomCarousel title={"Certificados"} items={certificates}/>
-			
-			{/* <Reviews /> */}
-			{/* <Similar /> */}
-			{/* <Comments/> */}
+			{/* future feature */}
+			{/* <CustomCarousel title={"Portfólio"} items={portfolio}/> */}
+			{/* <CustomCarousel title={"Certificados"} items={certificates}/> */}
+
+			<Reviews />
+			<Comments/>
 
 		</div>
 	);
