d3a26344c02232a2685fcfba001fa74a3338fa22
Esqulo <zecazeca11@hotmail.com>
2025-04-27 01:06:24 -0300

    update trustee component

--- src/components/TrusteeComponent/index.js
+++ src/components/TrusteeComponent/index.js
@@ -1,24 +1,27 @@
 import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import "./styles.css";
 
 import CustomImgComponent from "../CustomImgComponent";
 import Chevron from '../Chevron';
 
+import noUserImage from "../../assets/images/icons/no-image-profile.png";
+
 import StarIcon from "../../assets/images/icons/star.png"
 
 function TrusteeComponent({trusteeData}){
     const [chevronColor, setchevronColor] = useState("#555");
+    const navigate = useNavigate();
     return (
         <div className="tsc-container row-centered">
-            <CustomImgComponent img={trusteeData.img} width={"180px"} height={"180px"} borderRadius={"50%"}/>
+            <CustomImgComponent img={trusteeData.img || noUserImage} width={"180px"} height={"180px"} borderRadius={"50%"}/>
             <div className="tsc-content row-centered">
                 <div className="tsc-left-content">
                     <div className="tsc-info">
                         <div className="tsc-topcontent">
                             {trusteeData.sponsored && <span className="tsc-sponsor">Anúncio</span>}
                             <div className="tsc-description row-centered">
-                                <h1 className="tsc-name">{trusteeData.name}</h1>
+                                <h1 className="tsc-name" onClick={()=>{navigate(`/profile/${trusteeData.id}`)}}>{trusteeData.name}</h1>
                                 <div className="tsc-stars-container row-centered">
                                     <CustomImgComponent img={StarIcon} width={"20px"} height={"20px"}/>
                                     <span className="tsc-stars-value">{trusteeData.stars}</span>
@@ -26,7 +29,7 @@ function TrusteeComponent({trusteeData}){
                                 </div>
                             </div>
                             <div className="tsc-details">
-                                {trusteeData.experienceYears && <span>{trusteeData.experienceYears} anos como síndico profissional</span>}
+                                {trusteeData.experienceYears && <span>{trusteeData.experienceYears} ano{trusteeData.experienceYears > 1 ? 's' : ''} como síndico profissional</span>}
                                 {trusteeData.career && <span>Formado em {trusteeData.career}</span>}
                                 {(trusteeData.distance || trusteeData.age) &&
                                     <div className="tsc-details-row row-centered">
@@ -48,17 +51,14 @@ function TrusteeComponent({trusteeData}){
                     </div>
                 </div>
                 <div className="tsc-right-content">
-                    {trusteeData.username &&
-                                <Link to="/profile"state={{ id_user: trusteeData.name }}>
-                                    
-                            <div className="tsc-see-profile" onMouseEnter={()=>setchevronColor("#000")} onMouseLeave={()=>setchevronColor("#555")}>
-                                <span>ver perfil</span>
-                                <div className="tsc-chevron-container">
-                                    <Chevron color={chevronColor}/>
-                                </div>
+                    <Link to={`/profile/${trusteeData.id}`} state={{ id_user: trusteeData.name }}>
+                        <div className="tsc-see-profile" onMouseEnter={()=>setchevronColor("#000")} onMouseLeave={()=>setchevronColor("#555")}>
+                            <span>ver perfil</span>
+                            <div className="tsc-chevron-container">
+                                <Chevron color={chevronColor}/>
                             </div>
-                            </Link>
-                    }
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
