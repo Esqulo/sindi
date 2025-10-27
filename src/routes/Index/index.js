6535ab56dcc02a19628f827528550263c5b4f715
Esqulo <zecazeca11@hotmail.com>
2025-10-27 00:46:50 -0300

    add responsive home banner

--- src/routes/Index/index.js
+++ src/routes/Index/index.js
@@ -1,8 +1,22 @@
 import React from "react";
+import './styles.css';
+
+import bannerImage from "../../assets/images/home_banner.jpg";
 
 export default function Index(){
 
     //on start check for token and send to /home if necessary
 
-    return <h1>Hello World</h1>
+    const bannerBackground = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${bannerImage})`;
+
+    return (
+        <div className="index_container">
+            <div className="index_bannner_container" style={{backgroundImage: bannerBackground}}>
+                <div className="banner_text">
+                    <span className="highlight_text">Com a <b>Sindi</b>, conectar  síndicos e condomínios ficou <b>muito</b> mais fácil</span>
+                    <span className="description_text">Cadastre-se e tenha acesso direto a síndicos experientes ou ofereça seus serviços com visibilidade, credibilidade e segurança.</span>
+                </div>
+            </div>
+        </div>
+    )
 }
