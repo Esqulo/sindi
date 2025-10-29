1fb3c6d9408e165feb24c1fee3019e006f40ca40
Esqulo <zecazeca11@hotmail.com>
2025-10-28 21:19:46 -0300

    create Laptop component

---
+++ src/components/Laptop/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./styles.css";
+
+import laptopFrame from "../../assets/images/landing_page/laptop.png"
+
+function Laptop({ content = [], time = 5000 }){
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [fade, setFade] = useState(true);
+
+    useEffect(() => {
+        if (!content || content.length <= 1) return;
+
+        const interval = setInterval(() => {
+            setFade(false);
+            
+            setTimeout(() => {
+                setCurrentIndex((prevIndex) => (prevIndex + 1) % content.length);
+                setFade(true);
+            }, 500);
+            
+        }, time);
+
+        return () => clearInterval(interval);
+    }, [content, time]);
+
+    const currentContent = content[currentIndex];
+
+    if (!content || content.length === 0) return null;
+    return (
+        <div className="laptop_container">
+            <img className="laptop_frame" src={laptopFrame} alt="laptop-frame"/>
+            {currentContent.type === "image" &&
+                <img className={`laptop_screen ${fade ? 'fade-in' : 'fade-out'}`} src={currentContent.media} alt="laptop-screen" />
+            }
+            {currentContent.type === "video" &&
+                <video className={`laptop_screen ${fade ? 'fade-in' : 'fade-out'}`} src={currentContent.media} muted={true} autoPlay={true} loop playsInline controls={false}/>
+            }
+        </div>
+    );
+}
+
+export default Laptop;
